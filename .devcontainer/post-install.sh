92e4c268151b507c02781a1c71557f1a2afd1cd3
David Illing <dilling@users.noreply.github.com>
2023-04-22 21:19:34 +0000

    update paths

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -11,6 +11,8 @@ echo 'alias l="ls -CF"' >> $HOME/.bashrc
 # Convenience workspace directory for later use
 WORKSPACE_DIR=$(pwd)
 
+cd api
+
 # Change some Poetry settings to better deal with working in a container
 poetry config cache-dir ${WORKSPACE_DIR}/.cache
 poetry config virtualenvs.in-project true
